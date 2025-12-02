09ceb2c3e2d034f6eb9bd50cb9ffe6b4e9d875a7
Dorian Da Costa Cruz <dorian.dacostacruz@gmail.com>
2025-12-02 02:13:26 +0100

    dist deleted

--- dist/middleware/notFound.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFound = notFound;
-function notFound(_req, res) {
-    res.status(404).json({ error: 'NotFound' });
-}
